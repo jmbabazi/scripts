00c9dc85371d070add05ce5ac4c4e04c8a93423a
James Mbabazi <jmbabazi@pih.org>
2017-09-05 12:34:07 -0400

    Vim commands to replace words in a file
    This was created for replace word for audiometry stored procedures

---
+++ vim_cmd.sh
@@ -0,0 +1,39 @@
+vim -E -s  eth_l8000Hz_data.csv  << EOF
+:%s/ETH/call insert_audio_numeric_data('ETH/g
+:%s/,/','/g
+:%s/$/);/g
+:update
+:quit
+EOF
+
+vim -E -s  eth_r1000Hz_data.csv  << EOF
+:%s/ETH/call insert_audio_numeric_data('ETH/g
+:%s/,/','/g
+:%s/$/);/g
+:update
+:quit
+EOF
+
+vim -E -s  eth_r2000Hz_data.csv  << EOF
+:%s/ETH/call insert_audio_numeric_data('ETH/g
+:%s/,/','/g
+:%s/$/);/g
+:update
+:quit
+EOF
+
+vim -E -s  eth_r4000Hz_data.csv  << EOF
+:%s/ETH/call insert_audio_numeric_data('ETH/g
+:%s/,/','/g
+:%s/$/);/g
+:update
+:quit
+EOF
+
+vim -E -s  eth_r8000Hz_data.csv  << EOF
+:%s/ETH/call insert_audio_numeric_data('ETH/g
+:%s/,/','/g
+:%s/$/);/g
+:update
+:quit
+EOF
