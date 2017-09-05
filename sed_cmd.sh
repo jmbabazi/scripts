fb1f952ef8408191ff8e812836d39f7d3c692454
James Mbabazi <jmbabazi@pih.org>
2017-09-05 12:35:50 -0400

    sed command to replace/delete the nth character in a line

---
+++ sed_cmd.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+sed -i 's/.//61' eth_l8000Hz_data.csv
+sed -i 's/.//61' eth_r1000Hz_data.csv
+sed -i 's/.//61' eth_r2000Hz_data.csv
+sed -i 's/.//61' eth_r4000Hz_data.csv
+sed -i 's/.//61' eth_r8000Hz_data.csv
+cp eth_l1000Hz_data.csv eth_l1000Hz_data.sql
+cp eth_l2000Hz_data.csv eth_l2000Hz_data.sql
+cp eth_l4000Hz_data.csv eth_l4000Hz_data.sql
+cp eth_l8000Hz_data.csv eth_l8000Hz_data.sql
+cp eth_r1000Hz_data.csv eth_r1000Hz_data.sql
+cp eth_r2000Hz_data.csv eth_r2000Hz_data.sql
+cp eth_r4000Hz_data.csv eth_r4000Hz_data.sql
+cp eth_r8000Hz_data.csv eth_r8000Hz_data.sql
